0ab2ecea90bbb986ffd7094ffb3e75ead52a77b8
Theo Kobb <tkobb@e1z1r6p14.42.us.org>
2018-08-13 19:48:37 -0700

    read

--- read_map.c
+++ read_map.c
@@ -6,11 +6,12 @@
 /*   By: tkobb <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 15:03:48 by tkobb             #+#    #+#             */
-/*   Updated: 2018/08/13 15:09:41 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/08/13 19:48:21 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "read_map.h"
+#include "llist.h"
 #include "lib.h"
 
 static map		*read_meta(int filedes)
@@ -35,24 +36,43 @@ static map		*read_meta(int filedes)
 	map->full = *cur_buf++;
 	return (map);
 }
-#include <stdio.h>
+
+#define CHUNK_SIZE 32
+
+t_llist			*read_first_line(int filedes)
+{
+	char		*buf;
+	int			total_read
+	t_llist		*head;
+	t_llist		*tail;
+
+	head = llist_create_element(malloc(CHUNK_SIZE));
+	tail = head;
+	while (read(filedes, (char*)tail->data, 1) > 0)
+	{
+		if (*(char*)tail->data == '\n')
+			break ;
+		if (++len == CHUNK_SIZE)
+		{
+			tail->next = llist_create_element(malloc(CHUNK_SIZE));
+			tail = tail->next;
+		}
+	}
+	return (head);
+}
+
 map				*read_map(int filedes)
 {
 	map		*map;
-	//int		buf_size;
-	//int		num_read;
-	//char	*buf;
-	char	first_line[16];
-	int		i;
+//	int		buf_size;
+//	int		num_read;
+//	char	*buf;
+	char	*first_line;
 
-	i = 0;
 	map = read_meta(filedes);
-	while ((read(filedes, first_line, 1)))
-		if (first_line[i] == '\n')
-			break ;
-		else
-			i++;
-	map->width = i - 1;
+	first_line = read_first_line(filedes, &map->width);
+	map->tab = malloc(map->width * map->height * sizeof(char));
+	copy_first_line(first_line, map->tab, map->width);
 	return (map);
 }
 
