b55c134dc3090254a82fcd44b5db2d2e8d5bba4f
Theo Kobb <tkobb@e1z1r6p14.42.us.org>
2018-08-13 19:49:20 -0700

    list

---
+++ llist.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   llist.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkobb <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/13 19:22:17 by tkobb             #+#    #+#             */
+/*   Updated: 2018/08/13 19:48:48 by tkobb            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <llist.h>
+
+t_llist		*llist_create_element(void* data)
+{
+	t_llist		list;
+
+	if ((list = malloc(sizeof(t_llist))) == 0)
+		return (NULL);
+	list->next = NULL;
+	list->data = data;
+	return (list);
+}
---
+++ llist.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkobb <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/13 19:20:19 by tkobb             #+#    #+#             */
+/*   Updated: 2018/08/13 19:48:50 by tkobb            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LLIST_H
+# define LLIST_H
+# include <stdlib.h>
+
+typedef struct		s_llist
+{
+	void			*data;
+	struct s_list	*next;
+}					t_llist
+
+t_llist				*llist_create_element(void* data);
+
+#endif
