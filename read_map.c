f2145b490bd45c64e70d44b533c65d2b919444b8
Theo Kobb <tkobb@e1z1r6p14.42.us.org>
2018-08-13 23:22:28 -0700

    studf

--- read_map.c
+++ read_map.c
@@ -19,7 +19,7 @@ static t_map	*read_meta(int filedes)
 	if ((map = malloc(sizeof(t_map))) == 0)
 		/*error("malloc map")*/;
 	cur_buf = buf;
-	map->height = strptoi(&cur_buf);	
+	map->height = strptoi(&cur_buf);
 	map->empty = *cur_buf++;
 	map->obstacle = *cur_buf++;
 	map->full = *cur_buf++;
@@ -64,35 +64,45 @@ void			copy_first_line(t_llist *list, char *buf, int width)
 	curr = list;
 	while (i < width)
 	{
-		buf[i]= ((char*)curr->data)[i % CHUNK_SIZE];
+		buf[i] = ((char*)curr->data)[i % CHUNK_SIZE];
 		if (++i == CHUNK_SIZE)
 			curr = curr->next;
 	}
 }
 
+static char			transform(char c, t_map *map)
+{
+	if (c == map->empty)
+		return (0);
+	else if (c == map->obstacle)
+		return (1);
+	//error
+	return (3);
+}
+
 t_map			*read_map(int filedes)
 {
 	t_map	*map;
-//	int		buf_size;
-//	int		num_read;
-//	char	*buf;
 	t_llist	*first_line;
-
+	char	*buf;
+	int		x;
+	int		y;
+	
 	map = read_meta(filedes);
 	first_line = read_first_line(filedes, &map->width);
 	map->tab = malloc(map->width * map->height * sizeof(char));
 	copy_first_line(first_line, map->tab, map->width);
-	printf("%s\n", map->tab);
+	buf = malloc(map->width * sizeof(char));
+	y = 0;
+	while (y < map->height && read(filedes, buf, map->width))
+	{
+		x = 0;
+		while (x < map->width)
+		{
+			map->tab[x + map->width * y] = transform(buf[x], map);
+			x++;
+		}
+		y++;
+	}
 	return (map);
-}
-
-int main(int argc, char **argv)
-{
-	t_map *m = read_map(open(argv[1], O_RDONLY));
-	printf("w = %d\t", m->width);
-	printf("h = %d\t", m->height);
-	printf("empty = %c\t", m->empty);
-	printf("obstacle = %c\t", m->obstacle);
-	printf("full = %c\n", m->full);
-	(void)argc;
 }
