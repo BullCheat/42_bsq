b07211c1e2b8641e44e99182f5658182c7d39efd
Alexandre Drean <adrean@e1z1r2p40.42.us.org>
2018-08-15 12:58:41 -0700

    new algorithm

--- solver.c
+++ solver.c
@@ -1,8 +1,10 @@
 // FIXME header
 
+#include <string.h>
 #include "solver.h"
 #define getCell(x, y, map) map->tab[x + y * map->width]
 #define min(a, b) a < b ? a : b
+#define max(a, b) a > b ? a : b
 
 t_solution *allocate_solution() {
 	t_solution *s = malloc(sizeof(t_solution));
@@ -49,9 +51,6 @@ t_solution *solve(t_map *map)
 			int endy = map->height - y;
 			int end = min(endx, endy); // Max diagonal distance we can go to
 			if (getCell(x, y, map)) continue; // if there's an obstacle
-			if (x == 48 && y == 26) {
-				1 + 1;
-			}
 			for (int i = 1; i <= end; i++) {
 				char a = check_vertical(x, y, i, map);
 				char b = check_horizontal(x, y, i, map);
@@ -66,6 +65,39 @@ t_solution *solve(t_map *map)
 					break;
 				}
 			}
+		}
+	}
+	return sol;
+}
+
+t_solution *solve_1(t_map *map)
+{
+	t_solution *sol = allocate_solution();
+	int *r = malloc((map->width + 1) * sizeof(int));
+	int temp = 0;
+	memset(r, 0, sizeof(int) * (map->width + 1));
+	for (int y = 0; y < map->height; y++) {
+		for (int x = 0; x < map->width; x++) {
+			int temp2 = r[x + 1];
+			char res = map->tab[x + (y) * map->width];
+			if (res) {
+				r[x + 1] = 0;
+			} else {
+				int last = r[x], last_up = r[x + 1], last_up_left = temp;
+				int a = last;
+				if (a > last_up)
+					a = last_up;
+				if (a > last_up_left)
+					a = last_up_left;
+				a++;
+				r[x + 1] = a;
+				if (a > sol->len) {
+					sol->len = a;
+					sol->x = x - a + 1;
+					sol->y = y - a + 1;
+				}
+			}
+			temp = temp2;
 		}
 	}
 	return sol;
--- solver.h
+++ solver.h
@@ -7,5 +7,6 @@
 #include <stdlib.h>
 
 t_solution *solve(t_map *map);
+t_solution *solve_1(t_map *map);
 
 #endif //BSQ_SOLVE_H
