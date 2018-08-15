7c96dfe3a5e08edcb3783551f7bcdad57d517965
Theo Kobb <tkobb@e1z1r6p14.42.us.org>
2018-08-15 10:17:12 -0700

    print_map

--- print_map.h
+++ print_map.h
@@ -6,13 +6,13 @@
 /*   By: tkobb <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 22:57:00 by tkobb             #+#    #+#             */
-/*   Updated: 2018/08/15 09:51:16 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/08/15 10:13:54 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINT_MAP_H
 # define PRINT_MAP_H
-# define BUF_SIZE 64
+# define BUF_SIZE 64000
 # include "map.h"
 
 void	print_map(t_map *map, t_solution *solution);
